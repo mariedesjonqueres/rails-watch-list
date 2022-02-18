f02c89963a7a51efb0639f61a596b1c135f7cd16
Marie Desjonqueres marie.desjonqueres96@gmail.com <marie.desjonqueres96@gmail.com>
2022-02-18 11:48:46 +0100

    app production

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,31 @@
 class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :comment, length: { minimum: 6 }
-  validates :movie_id, uniqueness: { scope: :list, message: "No can't do!"}
+  validates :movie_id, uniqueness: { scope: :list_id, message: "No can't do!"}
 end
